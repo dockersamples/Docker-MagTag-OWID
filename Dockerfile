f93272843933fcd08e5a2a3d549083193fe4d811
Shy <Shy@users.noreply.github.com>
2022-03-10 10:56:53 -0500

    Turned this into a compose learning example for myself

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-# syntax=docker/dockerfile:1
-FROM tiangolo/uwsgi-nginx:python3.8-alpine
-RUN apk --update add bash nano
-ENV STATIC_URL /static
-ENV STATIC_PATH /var/www/app/static
-COPY ./requirements.txt /var/www/requirements.txt
-RUN pip install -r /var/www/requirements.txt
---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return jsonify({"status": "api online"})
+
+
+@app.route("/iso_data/<iso>", methods=["GET"])
+def iso_data(iso):
+    vaccination_url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json"
+    try:
+
+        vaccinations = requests.get(vaccination_url)
+    except:
+        return "False"
+    vaccinations_dict = {}
+    for item in vaccinations.json():
+        if item["iso_code"]:
+            vaccinations_dict[item["iso_code"]] = {
+                "data": item["data"][-1],
+                "country": item["country"],
+            }
+    if iso in vaccinations_dict:
+        return jsonify(vaccinations_dict[iso])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
